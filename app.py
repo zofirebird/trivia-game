825841dfd0890b5df5bdc6182a696b25973d807e
rsreddy <radha21@gmail.com>
2017-05-08 13:06:57 +0000

    add more shell htmls

--- app.py
+++ app.py
@@ -8,4 +8,20 @@ def index():
     return render_template(
         'index.html')
 
+@app.route('/error')
+def error():
+    return render_template(
+        'error.html')
+
+@app.route('success')
+def success():
+    return render_template(
+        'success.html')
+
+@app.route('about')
+def about():
+    return render_template(
+        'about.html')
+
+
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
