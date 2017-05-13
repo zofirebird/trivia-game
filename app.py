81d5429751c0a92553b3af5a51f9b3225e4b5e40
rsreddy <radha21@gmail.com>
2017-05-13 17:57:07 +0000

    commit for index redirect

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+@app.route('/question')
+def question():
+    return render_template(
+        'questions/1.html')
+        
 @app.route('/')
 def index():
     return render_template(
@@ -22,6 +27,29 @@ def success():
 def about():
     return render_template(
         'about.html')
+        
+# @app.route('/questions/<string:answer>/<int:question_id')
+# def questions(answer, question_id ):
+  #  if question_id = 1
+  #      if answer = "blue"
+   #        return render_template(
+    #           'success.html')
+    #    elif
+    #      return render_template(
+    #         'error.html',
+    #          correct_answer = answer)
+    #elif question_id = 2
+     #   return render_template(
+      #      'questions/2.html')
+    #elif question_id = 3
+    #    return render_template(
+    #        'questions/3.html')
+#    elif question_id = 4 
+#        return render_template(
+#            'questions/4.html')
+#    elif question_id = 5
+#        return render_template(
+#            'questions/5.html')
 
 
 app.run(host=os.getenv('IP', '0.0.0.0'),port=int(os.getenv('PORT', 8080)))
